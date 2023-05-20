6a0379dafea52a876c46a0fe2bd93ef11c01354b
MithraV2009 <92360446+MithraV2009@users.noreply.github.com>
2023-05-20 13:47:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song="";
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -7,10 +11,36 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on('pose',gotposes);
+}
+function modelloaded(){
+    console.log("modelloaded")
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        console.log("left wrist x = "+leftwristx+" left wrist y = "+leftwristy);
+        console.log("right wrist x = "+rightwristx+" right wrist y = "+rightwristy);
+    }
+
 }
 function draw(){
     image(video,0,0,600,500);
-}
+    circle(leftwristx,leftwristy,20);
+    fill("red");
+    stroke("black");
+    innumberleftwristy=Number(leftwristy);
+    remove_decimal=floor(innumberleftwristy);
+    volume=remove_decimal/1000;
+    volume1=volume*2;
+    document.getElementById("volume").innerHTML="volume = "+volume1;
+    song.setVolume(volume1);
+    }
 function play(){
     song.play();
 }
